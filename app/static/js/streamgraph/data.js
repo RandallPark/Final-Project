cf7d823ba2b12f8b7716f7b9f944ca2dfe6e4134
phoshell <charlotte.avocado@gmail.com>
2019-06-29 10:45:59 -0700

    Adapting examples to Flask Jinja templates

---
+++ app/static/js/streamgraph/data.js
@@ -0,0 +1,23 @@
+obama = [{
+        "year": "Jun 19 2019",
+        "polarity": -0.12062211981566817,
+        "word_count": 20
+
+    },
+    {
+        "created_at": "Jun 25 2019",
+        "polarity": 0.17876984126984122,
+        "word_count": 10
+
+    },
+    {
+        "created_at": "Jun 18 2019",
+        "polarity": 0.025707735247208933,
+        "word_count": 25
+    },
+    {
+        "created_at": "March 18 2019",
+        "polarity": 0.09821881149153878,
+        "word_count": 18
+    }
+]
