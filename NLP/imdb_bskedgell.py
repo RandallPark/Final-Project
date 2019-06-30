b7023eaf4f2c6b36458cd369847f28bc97de8a6b
Bryan Skedgell <BSkedgell@gmail.com>
2019-06-30 10:42:04 -0700

    oh snap something might be weird hope this works

--- NLP/imdb_bskedgell.py
+++ NLP/imdb_bskedgell.py
@@ -1,3 +1,83 @@
+########################################################################
+#
+# Functions for downloading the IMDB Review data-set from the internet
+# and loading it into memory.
+#
+# Implemented in Python 3.6
+#
+# Usage:
+# 1) Set the variable data_dir with the desired storage directory.
+# 2) Call maybe_download_and_extract() to download the data-set
+#    if it is not already located in the given data_dir.
+# 3) Call load_data(train=True) to load the training-set.
+# 4) Call load_data(train=False) to load the test-set.
+# 5) Use the returned data in your own program.
+#
+# Format:
+# The IMDB Review data-set consists of 50000 reviews of movies
+# that are split into 25000 reviews for the training- and test-set,
+# and each of those is split into 12500 positive and 12500 negative reviews.
+# These are returned as lists of strings by the load_data() function.
+#
+########################################################################
+#
+# This file is part of the TensorFlow Tutorials available at:
+#
+# https://github.com/Hvass-Labs/TensorFlow-Tutorials
+#
+# Published under the MIT License. See the file LICENSE for details.
+#
+# Copyright 2018 by Magnus Erik Hvass Pedersen
+#
+########################################################################
+
+import os
+import download
+import glob
+
+########################################################################
+
+# Directory where you want to download and save the data-set.
+# Set this before you start calling any of the functions below.
+data_dir = "data/TWEETS_bskedgell/"
+
+# URL for the data-set on the internet.
+data_url = "data/TWEETS_bskedgell/"
+
+
+########################################################################
+# Private helper-functions.
+
+def _read_text_file(path):
+    """
+    Read and return all the contents of the text-file with the given path.
+    It is returned as a single string where all lines are concatenated.
+    """
+
+    with open(path, 'rt', encoding='utf-8') as file:
+        # Read a list of strings.
+        lines = file.readlines()
+
+        # Concatenate to a single string.
+        text = " ".join(lines)
+
+    return text
+
+
+########################################################################
+# Public functions that you may call to download the data-set from
+# the internet and load the data into memory.
+
+
+def maybe_download_and_extract():
+    """
+    Download and extract the IMDB Review data-set if it doesn't already exist
+    in data_dir (set this variable first to the desired directory).
+    """
+
+    download.maybe_download_and_extract(download_dir=data_dir, url=data_url)
+
+
 def load_data(train=True):
     """
     Load all the data from the IMDB Review data-set for sentiment analysis.
@@ -14,7 +94,7 @@ def load_data(train=True):
     train_test_path = "train" if train else "test"
 
     # Base-directory where the extracted data is located.
-    dir_base = os.path.join(data_dir, "aclImdb", train_test_path)
+    dir_base = os.path.join(data_dir, "acltweets", train_test_path)
 
     # Filename-patterns for the data-files.
     path_pattern_pos = os.path.join(dir_base, "pos", "*.txt")
@@ -36,3 +116,6 @@ def load_data(train=True):
     y = [1.0] * len(data_pos) + [0.0] * len(data_neg)
 
     return x, y
+
+
+########################################################################
