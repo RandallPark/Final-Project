55fb11ec4dcdff3f4efa27ea692d7c12ba7bdf77
Warren Ross <warren.r.ross@gmail.com>
2019-07-02 14:53:16 -0700

    Working on flask routes

--- app/app.py
+++ app/app.py
@@ -7,7 +7,9 @@
 from flask import (
     Flask,
     render_template,
-    request)
+    request,
+    redirect,
+    jsonify)
 
 
 #################################################
@@ -45,27 +47,72 @@ def tweetreport():
     return render_template("tweet-report.html")
 
 
-@app.route('/signup', methods = ['POST'])
-def signup():
-    email = request.form['email']
-    print("The email address is '" + email + "'")
-    return redirect('/')
+# @app.route('/example', methods = ['POST'])
+# def signup():
+#     email = request.form['email']
+#     print("The email address is '" + email + "'")
+#     return redirect('/')
+
+
+# Create a list to hold our data for EXAMPLE
+my_data = []
+
+#EXAMPLE
+@app.route("/api/data")
+def data():
+    print(my_data)
+    return jsonify(my_data)
+
+#EXAMPLE
+@app.route("/send", methods=["GET", "POST"])
+def send():
+    if request.method == "POST":
+        nickname = request.form["nickname"]
+        age = request.form["age"]
+
+        form_data = {
+            "nickname": nickname,
+            "age": int(age)
+        }
+
+        my_data.append(form_data)
+
+        return "Thanks for the form data!"
+
+    return render_template("form.html")
+
+#EXAMPLE
+@app.route('/input')
+def student():
+   return render_template('basic-input.html')
+
+#EXAMPLE
+@app.route('/output',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      print(result)
+      return render_template("basic-output.html",result = result)
+
+@app.route("/text-box", methods=['GET'])
+def get_text():
+    """ process the form """
+    # text_entry = request.form['text_entry']
+    # print("The tweet GET '" + text_entry + "'")
+    return render_template("text-box.html")
 
 # SENTIMENT SCORING INTERACTION
 @app.route("/text-box", methods=['POST'])
-def get_text():
-    """ Show form to user """
-    email = request.form['email']
-    print("The email address is '" + email + "'")
-    return render_template("text-box.html")
-
-@app.route("/text-box", methods=['GET'])
 def post_text():
-    """ process the form """
+    """ Show form to user """
+    # text_entry = ''
+    text_entry = request.form['text_entry']
+    print(f"The tweet POST {text_entry}")
     return render_template("text-box.html")
 
 
-# 4. Define main behavior
+
+# Define main behavior
 if __name__ == "__main__":
     app.run(debug=True)
 
